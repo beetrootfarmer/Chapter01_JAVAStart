98716b08703713c77e9437867c3530fcb0acd602
bitcamp <bitcamp@DESKTOP-MCTE6K8>
2021-07-28 09:47:33 +0900

    first commit from the bitcamp

---
+++ src/ch01_3_java_process/Hello.java
@@ -0,0 +1,28 @@
+package ch01_3_java_process;
+
+/***************************
+주석이 왜 필요할까요?
+소스코드를 설명해주기 위해!
+작성자:
+연락처:
+작성일:
+저작권:
+프로젝트정보:::
+
+ ***************************/
+public class Hello {
+
+	public static void main(String[] args) {
+		/*
+		 * 이 method에 대한 설명~
+		 * arg(입력)
+		 * 출력
+		 * 내용(기능)
+		 */
+		// TODO Auto-generated method stub
+		System.out.println("반갑습니다!");
+//System.out.append(c);
+//		System.out.println();
+//	}sy();
+	}
+}
---
+++ src/ch01_3_java_process/RunStatementExample.java
@@ -0,0 +1,14 @@
+package ch01_3_java_process;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result =x+y;
+		
+		System.out.println(result);
+	}
+	public static void method1() {
+		System.out.println("Hello+++");
+		}
+}
